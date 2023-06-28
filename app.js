4133d7b28ca90c8cf60df372572579af146fe8be
Raeann Lytle <raelytle@gmail.com>
2023-06-28 18:40:09 -0400

    start over sprint 12

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 const { PORT = 3001 } = process.env;
 app.listen(PORT, () => {
